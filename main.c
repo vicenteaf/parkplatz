50761d53de42c91244d688561eb881799c1b88d4
madkep <felipecespedes97@gmail.com>
2017-06-05 19:42:47 -0400

    Vicho chupalo
    Agregué el menu qlo

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "menu.h"
 
 int main(){
-    
+
     mainMenu();
-           
+    return 0;
 }
-    
--- menu.c
+++ menu.c
@@ -1,24 +1,32 @@
 void mainMenu(){
-     int  opc;
-     char opcstr[3];                
-         
-          printf("1.- Angregar Auto \n2.- Eliminar Auto\n3.- Buscar Auto ");
-          fgets(opc,3,stdin);
-          opc = atoi(opcstr);
-          
-          switch(opc){
-                      
-                  case 1:
-                       addCar();
-                       break;
-                  case 2:
-                       delCar();
-                       break;
-                  case 3:    
-                       schCar();
-                       break;
-                  default:
-                      
-          }
+     int  opc=0;
+     char opcstr[3];
+
+
+          while(opc!=4){
+            printf("1.- Agregar Auto \n2.- Eliminar Auto\n3.- Buscar Auto\n4.- Salir Del Programa\n\n");
+            fgets(opcstr,3,stdin);
+            opc = atoi(opcstr);
+            switch(opc){
+
+                    case 1:
+                         //addCar();
+                         printf("hola\n");
+                         system("cls");
+                         break;
+
+                    case 2:
+                         //delCar();
+                         system("cls");
+                         break;
+                    case 3:
+                         //schCar();
+                         system("cls");
+                         break;
+                    default:
+                          system("cls");
+                          break;
+
+            }
+      }
 }
-     
